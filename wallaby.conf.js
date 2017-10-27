2dfc1b93cbe79b1d0de918da1ec845924564d1ac
Artem Govorov <artem.govorov@gmail.com>
2017-10-27 12:23:49 +1000

    Update wallaby.conf.js

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -6,7 +6,7 @@ var wallabyPostprocessor = wallabyWebpack({
   resolve: {
     alias: {
       'kendo-ui/js': '@progress/kendo-ui/js',
-      'kendo-ui': '/kendo-ui-core/js/kendo.core.js',
+      'kendo-ui': 'kendo-ui-core/js/kendo.core.js',
       'module': 'webpack/lib/Module.js'
     }
   },
