a34a0acc8e6d55d8b2b86ced751fb8bd4e1edbb8
Artem Govorov <artem.govorov@gmail.com>
2017-10-28 13:22:37 +1000

    Update app.spec.ts

--- test/unit/app.spec.ts
+++ test/unit/app.spec.ts
@@ -27,8 +27,7 @@ describe('the app', () => {
     component.bootstrap(aurelia => {
       aurelia.use
         .standardConfiguration()
-        .plugin(PLATFORM.moduleName('aurelia-kendoui-bridge/notification/notification.html') &&
-          PLATFORM.moduleName("aurelia-kendoui-bridge"));
+        .plugin(PLATFORM.moduleName("aurelia-kendoui-bridge"));
     });
 
   });
